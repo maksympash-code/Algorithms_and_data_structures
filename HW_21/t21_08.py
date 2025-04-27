97916d724db19d144014bc32966c6c2ca66cb802
maksympash-code <maksympash@knu.ua>
2025-04-27 20:02:53 +0300

    HW_21

--- HW_21/t21_08.py
+++ HW_21/t21_08.py
@@ -1,34 +1,27 @@
 class Graph:
-    def __init__(self, oriented=True, v = 0):
+    def __init__(self, oriented=True, v=0):
         self.oriented = oriented
         self.v = v
-
         self.graph = [[0] * v for _ in range(v)]
 
-    def addEdge(self, s, d, w = 1):
+    def addEdge(self, s, d, w=1):
         self.graph[s][d] += w
-
         if not self.oriented:
             self.graph[d][s] += w
 
     def multiEdges(self):
-
         for i in range(self.v):
             for j in range(self.v):
                 if self.graph[i][j] > 1:
                     return True
         return False
 
-
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    g = Graph(True, m)
+    g = Graph(oriented=True, v=n)
 
     for _ in range(m):
         u, v = map(int, input().split())
         g.addEdge(u - 1, v - 1)
 
-    if g.multiEdges():
-        print('YES')
-    else:
-        print('NO')
+    print("YES" if g.multiEdges() else "NO")
